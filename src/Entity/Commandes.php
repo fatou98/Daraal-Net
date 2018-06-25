01978a32c897f5963d645048c0b5b490a618ca6d
fatou98 <mbathiefatou601@gmail.com>
2018-06-25 21:35:34 +0000

    Creation de la base de données

---
+++ src/Entity/Commandes.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommandesRepository")
+ */
+class Commandes
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Date;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="App\Entity\Utilisateurs")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $utilisateur;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="App\Entity\Betes")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $bete;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $Etat;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getEtat(): ?bool
+    {
+        return $this->Etat;
+    }
+
+    public function setEtat(bool $Etat): self
+    {
+        $this->Etat = $Etat;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of utilisateur
+     */ 
+    public function getUtilisateur()
+    {
+        return $this->utilisateur;
+    }
+
+    /**
+     * Set the value of utilisateur
+     *
+     * @return  self
+     */ 
+    public function setUtilisateur($utilisateur)
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of bete
+     */ 
+    public function getBete()
+    {
+        return $this->bete;
+    }
+
+    /**
+     * Set the value of bete
+     *
+     * @return  self
+     */ 
+    public function setBete($bete)
+    {
+        $this->bete = $bete;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Races.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RacesRepository")
+ */
+class Races
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Libelle;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->Libelle;
+    }
+
+    public function setLibelle(string $Libelle): self
+    {
+        $this->Libelle = $Libelle;
+
+        return $this;
+    }
+}
