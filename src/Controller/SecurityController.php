b8516c8f6c7a0496f923270eb949255e4c4adc41
Gueye1890 <jaafargueye@gmail.com>
2018-06-28 23:51:48 +0000

    modification mo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 namespace App\Controller;
 
 use App\Form\UserType;
@@ -40,7 +39,7 @@ class SecurityController extends Controller {
     }
     /**
      * @Route("/inscriptionBerger",name="inscriptionBerger")
-     */
+     *//*
     public function registerBergerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
         // 1) build the form
         $user = new User();
@@ -67,60 +66,21 @@ class SecurityController extends Controller {
     }
     /**
      * @Route("/inscriptionAdmin",name="inscriptionAdmin")
-     */
+     *//*
     public function registerAdminAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
         // 1) build the form
         $user = new User();
-=======
-
-namespace App\Controller;
-
-use App\Entity\Utilisateurs;
-use App\Form\UserType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-class SecurityController extends Controller
-{
-    /**
-     * @Route("/security", name="security")
-     */
-    public function index()
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
-    /**
-     * @Route("/inscription")
-     */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
-        // 1) build the form
-        $user = new Utilisateurs();
->>>>>>> 2b1363964c25aa7d6b18bf99d5eba3e54b62b014
         $form = $this->createForm(UserType::class, $user);
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             // 3) Encode the password (you could also do this via Doctrine listener)
-<<<<<<< HEAD
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
             //on active par défaut
             $user->setIsActive(true);
             $user->addRole("ROLE_SUPER_ADMIN");
             
-=======
-            $password = $user->getPlainPassword();
-            $user->setPassword($password);
-            //on active par défaut
-            //$user->setIsActive(true);
-            //
-       /// $user->addRole(roles);
->>>>>>> 2b1363964c25aa7d6b18bf99d5eba3e54b62b014
             // 4) save the User!
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
@@ -128,19 +88,11 @@ class SecurityController extends Controller
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
             $this->addFlash('success', 'Votre compte à bien été enregistré.');
-<<<<<<< HEAD
             return $this->redirectToRoute('login');
         }
         return $this->render('security/inscription.html.twig', ['form' => $form->createView(), 'mainNavRegistration' => true, 'title' => 'Inscription']);
-    }
+    }*/
      /**
-=======
-            //return $this->redirectToRoute('login');
-        }
-        return $this->render('security/register.html.twig', ['form' => $form->createView(), 'mainNavRegistration' => true, 'title' => 'Inscription']);
-    }
-/**
->>>>>>> 2b1363964c25aa7d6b18bf99d5eba3e54b62b014
      * @Route("/login", name="login")
      */
     public function login(Request $request, AuthenticationUtils $authenticationUtils) {
@@ -153,15 +105,10 @@ class SecurityController extends Controller
                 ->createNamedBuilder(null)
                 ->add('_username', null, ['label' => 'Email'])
                 ->add('_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Mot de passe'])
-<<<<<<< HEAD
                 ->add('connexion', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Connexion', 'attr' => ['class' => 'green darken-4']])
                 ->getForm();
 //return $this->redirectToRoute('membre');
 
-=======
-                ->add('ok', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Ok', 'attr' => ['class' => 'btn-primary btn-block']])
-                ->getForm();
->>>>>>> 2b1363964c25aa7d6b18bf99d5eba3e54b62b014
         return $this->render('security/login.html.twig', [
                     'mainNavLogin' => true, 'title' => 'Connexion',
                     //
@@ -170,10 +117,7 @@ class SecurityController extends Controller
                     'error' => $error,
         ]);
     }
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> 2b1363964c25aa7d6b18bf99d5eba3e54b62b014
 }
